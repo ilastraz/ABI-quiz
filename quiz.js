934b247e13ae599e433de88b56009bcfbecf0c11
ilastraz <38754098+ilastraz@users.noreply.github.com>
2024-11-06 23:05:29 +0100

    quiz update

--- quiz.js
+++ quiz.js
@@ -1,14 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
   const startButton = document.querySelector(".quiz-start-button");
-  const quizStart = document.querySelector(".quiz-start");
-  const quizAnswerWrapper = document.querySelector(".quiz-answer-wrapper");
-
-  if (startButton) {
+  if (startButton !== null) {
     startButton.addEventListener("click", function () {
+      const quizStart = document.querySelector(".quiz-start");
+      const quizAnswerWrapper = document.querySelector(".quiz-answer-wrapper");
       if (quizStart && quizAnswerWrapper) {
         quizStart.style.display = "none";
         quizAnswerWrapper.style.display = "block";
       }
     });
+  } else {
+    console.error("Elemento .quiz-start-button non trovato nel DOM.");
   }
 });
